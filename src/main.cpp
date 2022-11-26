e70e449f97d7ea0a1b7f40e9065442847c662ff7
Tarik <Tarik.Kalai@ulb.be>
2022-11-26 16:12:51 +0100

    starting the set up of opengl in Vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,248 @@
-int main(){
-    return 0; 
+#include<iostream>
+
+//include glad before GLFW to avoid header conflict or define "#define GLFW_INCLUDE_NONE"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <chrono>
+#include <thread>
+
+
+const int width = 500;
+const int height = 500;
+
+
+GLuint compileShader(std::string shaderCode, GLenum shaderType);
+GLuint compileProgram(GLuint vertexShader, GLuint fragmentShader);
+
+
+
+#ifndef NDEBUG
+void APIENTRY glDebugOutput(GLenum source,
+	GLenum type,
+	unsigned int id,
+	GLenum severity,
+	GLsizei length,
+	const char* message,
+	const void* userParam)
+{
+	// ignore non-significant error/warning codes
+	if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+	std::cout << "---------------" << std::endl;
+	std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+	switch (source)
+	{
+	case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+	case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+	case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+	} std::cout << std::endl;
+
+	switch (type)
+	{
+	case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+	case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+	case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+	case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+	case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+	case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+	case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+	} std::cout << std::endl;
+
+	switch (severity)
+	{
+	case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+	case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+	case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+	} std::cout << std::endl;
+	std::cout << std::endl;
+}
+#endif
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Welcome to exercice Four: FPS counter" << std::endl;
+	std::cout << "Let's count how many frames per second (FPS) you have" << std::endl;
+	std::cout << "1. Make a function that compute the FPS at each loop of the animation" << std::endl;
+	std::cout << "Hint: Take a look at glfwGetTime()" << std::endl;
+	std::cout << "2. Check the refresh rate of your screen, is it similar ?" << std::endl;
+
+	//Boilerplate
+	if (!glfwInit()) {
+		throw std::runtime_error("Failed to initialise GLFW \n");
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifndef NDEBUG
+	//create a debug context to help with Debugging
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+#endif
+
+
+	//1.b Create the window
+	GLFWwindow* window = glfwCreateWindow(width, height, "Solution 04", nullptr, nullptr);
+	if (window == NULL)
+	{
+		glfwTerminate();
+		throw std::runtime_error("Failed to create GLFW window\n");
+	}
+
+	glfwMakeContextCurrent(window);
+	//load openGL function
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		throw std::runtime_error("Failed to initialize GLAD");
+	}
+
+#ifndef NDEBUG
+	int flags;
+	glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+	if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+	{
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(glDebugOutput, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+	}
+#endif
+
+	const std::string sourceV = "#version 330 core\n"
+		"layout(location = 0) in vec3 position; \n"
+		" void main(){ \n"
+		"gl_Position = vec4(position, 1);\n"
+		"}\n"; 
+	const std::string sourceF = "#version 330 core\n"
+		"precision mediump float; \n"
+		"out vec4 fragColor;\n"
+		"void main() { \n"
+		" fragColor = vec4(1.0, 0.0, 0.0, 1.0); \n"
+		"} \n";
+
+	
+	
+	GLuint shaderV = compileShader(sourceV, GL_VERTEX_SHADER);
+	GLuint shaderF = compileShader(sourceF, GL_FRAGMENT_SHADER);
+	GLuint program = compileProgram(shaderV, shaderF);
+
+	// First object!
+	const float positionsData[9] = {
+		// vertices
+		-1.0, -1.0, 0.0,
+		1.0, -1.0, 0.0,
+		0.0, 1.0, 0.0,
+	};
+
+	//Create the buffer
+
+	//TODO explain why we need a VAO AND a VBO
+	GLuint VBO, VAO;
+	//generate the buffer and the vertex array
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	//define VBO and VAO as active buffer and active vertex array
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positionsData), positionsData, GL_STATIC_DRAW);
+
+	auto attribute = glGetAttribLocation(program, "position");
+	glEnableVertexAttribArray(attribute);
+	glVertexAttribPointer(attribute, 3, GL_FLOAT, false, 0, 0);
+	//desactive the buffer
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	double prev = 0;
+	int deltaFrame = 0;
+	//there is multiple way to implement the same solution,
+	//Here is an example using a lambda function
+	auto fps = [&](void) {
+		double now = glfwGetTime();
+		double deltaTime = now - prev;
+		deltaFrame++;
+		if (deltaTime > 0.5) {
+			prev = now;
+			const double fpsCount = (double) deltaFrame / deltaTime;
+			deltaFrame = 0;
+			std::cout << "\r FPS: " << fpsCount;
+			std::cout.flush();
+		}
+	};
+
+	//sync with the screen refresh rate
+	glfwSwapInterval(1);
+	//Rendering
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+
+		glUseProgram(program);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//"1. Make a function that compute the FPS at each loop of the animation"
+		fps();
+		glfwSwapBuffers(window);
+	}
+
+	//clean up ressource
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
+
+GLuint compileShader(std::string shaderCode, GLenum shaderType)
+{
+	GLuint shader = glCreateShader(shaderType);
+	const char* code = shaderCode.c_str();
+	glShaderSource(shader, 1, &code, NULL);
+	glCompileShader(shader);
+
+	GLchar infoLog[1024];
+	GLint success;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+		std::string t = "undetermined";
+		if (shaderType == GL_VERTEX_SHADER) {
+			t = "vertex shader";
+		}
+		else if (shaderType == GL_FRAGMENT_SHADER) {
+			t = "fragment shader";
+		}
+		std::cout << "ERROR::SHADER_COMPILATION_ERROR of the " << t << ": " << shaderType << infoLog << std::endl;
+	}
+	return shader;
+}
+
+GLuint compileProgram(GLuint vertexShader, GLuint fragmentShader)
+{
+	GLuint programID = glCreateProgram();
+
+	glAttachShader(programID, vertexShader);
+	glAttachShader(programID, fragmentShader);
+	glLinkProgram(programID);
+
+
+	GLchar infoLog[1024];
+	GLint success;
+	glGetProgramiv(programID, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(programID, 1024, NULL, infoLog);
+		std::cout << "ERROR::PROGRAM_LINKING_ERROR:  " << infoLog << std::endl;
+	}
+	return programID;
 }
