f22627c46d11bf0c67a34c5cf63b78f5474f5c57
Aurelien <aurelien.chassagne@ulb.be>
2022-12-03 17:24:54 +0100

    cut cubemap to 6 images

---
+++ src/utils/cut_cubemap.py
@@ -0,0 +1,26 @@
+from PIL import Image 
+
+PATH = "/home/aurelien/Téléchargements/"
+filename = "cube2persp1.jpg"
+
+img = Image.open(f"{PATH}{filename}") 
+width, height = img.size
+
+block_size = (width//4, height//3)
+
+for i in range(4):
+    for j in range(3):
+
+        img_res = img.crop((i*block_size[0], (j)*block_size[1], (i+1)*block_size[0], (j+1)*block_size[1])) 
+        if i==1 and j==0: # Top
+            img_res.save(f"{PATH}posy.jpg")
+        if i==0 and j==1: # Left
+            img_res.save(f"{PATH}negx.jpg")
+        if i==1 and j==1: # Center
+            img_res.save(f"{PATH}posz.jpg")
+        if i==2 and j==1: # Right
+            img_res.save(f"{PATH}posx.jpg")
+        if i==3 and j==1: # Right++
+            img_res.save(f"{PATH}negz.jpg")
+        if i==1 and j==2: # Bottom
+            img_res.save(f"{PATH}negy.jpg")
