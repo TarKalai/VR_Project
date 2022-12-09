48662cac7716b08d4c2ad45391f0afe9ff430229
sebadevo <sebastien.de.vos@ulb.be>
2022-12-09 17:53:49 +0100

    installation of bullet3 library

---
+++ build/src/CMakeFiles/SRC_main.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/seb/Documents/polytech/MA2/INFO-H502/VR_Project/src/main.cpp" "/home/seb/Documents/polytech/MA2/INFO-H502/VR_Project/build/src/CMakeFiles/SRC_main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../3rdParty/glad/include"
+  "../3rdParty/glfw/include"
+  "../3rdParty/glm"
+  "../3rdParty/stb"
+  "../3rdParty/bullet3/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/seb/Documents/polytech/MA2/INFO-H502/VR_Project/build/3rdParty/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/seb/Documents/polytech/MA2/INFO-H502/VR_Project/build/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/home/seb/Documents/polytech/MA2/INFO-H502/VR_Project/build/3rdParty/bullet3/src/BulletDynamics/CMakeFiles/BulletDynamics.dir/DependInfo.cmake"
+  "/home/seb/Documents/polytech/MA2/INFO-H502/VR_Project/build/3rdParty/bullet3/src/BulletCollision/CMakeFiles/BulletCollision.dir/DependInfo.cmake"
+  "/home/seb/Documents/polytech/MA2/INFO-H502/VR_Project/build/3rdParty/bullet3/src/LinearMath/CMakeFiles/LinearMath.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/SRC_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SRC_main.dir/main.cpp.o"
+  "SRC_main"
+  "SRC_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SRC_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
