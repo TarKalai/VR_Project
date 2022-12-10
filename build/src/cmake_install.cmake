3b0a66264e58ddd598abacaa86a63ecaea6c00e7
sebadevo <sebastien.de.vos@ulb.be>
2022-12-10 22:23:38 +0100

    objects added

--- build/src/cmake_install.cmake
+++ build/src/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/aurelien/Documents/MA2/VR/VR_Project/src
+# Install script for directory: /home/seb/Documents/polytech/MA2/INFO-H502/VR_Project/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,16 +29,11 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
-endif()
-
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
