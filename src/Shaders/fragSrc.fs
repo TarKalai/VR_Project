ad0f5420f3e5280cfd71b16589031f9d234675c3
Aurelien <aurelien.chassagne@ulb.be>
2023-01-07 11:31:50 +0100

    pre merge (not working version)

--- src/Shaders/fragSrc.fs
+++ src/Shaders/fragSrc.fs
@@ -1,13 +1,16 @@
-#version 330
+#version 330 core
 
 in vec4 vertexColor;
+in vec2 TexCoord; // will be interpollated if the points are not exactly on the expected points. //v_tex
 in vec3 Normal; 
-in vec3 FragPos; 
+in vec3 FragPos;
 
 out vec4 color;
 
+
 const int MAX_POINT_LIGHTS = 4;
-const int MAX_SPOT_LIGHTS = 3; 
+const int MAX_SPOT_LIGHTS = 3;
+const int MAX_AREA_LIGHTS = 100;
 
 struct Light
 {
@@ -28,7 +31,6 @@ struct PointLight
     float constant;
     float linear; 
     float exponent; 
-
 };
 
 struct SpotLight
@@ -38,23 +40,39 @@ struct SpotLight
     float edge;
 };
 
+struct AreaLight{
+    Light base;
+    vec3 points[4];
+    bool twoSided;
+};
+
 struct Material{
     float specularIntensity; 
     float shininess; 
+    sampler2D diffuse;
+    vec4 albedoRoughness; // (x,y,z) = color, w = roughness
 };
 
-uniform int pointLightCount; 
+uniform int pointLightCount;
 uniform int spotLightCount; 
+uniform int areaLightCount;
 
 uniform DirectionalLight directionalLight;
 uniform PointLight pointLights[MAX_POINT_LIGHTS];
-uniform SpotLight spotLights[MAX_SPOT_LIGHTS]; 
+uniform SpotLight spotLights[MAX_SPOT_LIGHTS];  
+uniform AreaLight areaLights[MAX_AREA_LIGHTS];
 
+uniform sampler2D theTexture; 
+uniform sampler2D LTC1; // for inverse M
+uniform sampler2D LTC2; // GGX norm, fresnel, 0(unused), sphere
+uniform Material material;
+uniform vec3 areaLightTranslate;
 
-// uniform sampler2D theTexture; 
-uniform Material material; 
+uniform vec3 eyePosition; // camera position (view position)
 
-uniform vec3 eyePosition; // camera position
+const float LUT_SIZE  = 64.0; // ltc_texture size
+const float LUT_SCALE = (LUT_SIZE - 1.0)/LUT_SIZE;
+const float LUT_BIAS  = 0.5/LUT_SIZE;
 
 vec4 CalcLightByDirection(Light light, vec3 direction) // in the point light calculation there is a part where we need to compute the light based on the direction, directional light calculate all of it by direction. 
 {
@@ -66,12 +84,12 @@ vec4 CalcLightByDirection(Light light, vec3 direction) // in the point light cal
 
     vec4 diffuseColor = vec4(light.color, 1.0f)*light.diffuseIntensity * diffuseFactor; // diffuseFactor check if the light is at an angle that can be allowed to appear. 
     
-    vec4 specularColor = vec4(0,0,0,0); 
+    vec4 specularColor = vec4(0.0, 0.0, 0.0, 0.0); 
 
     if(diffuseFactor>0.0f)
     {
         vec3 fragToEye = normalize(eyePosition - FragPos); // we just want the direction where the fragment is from the eye
-        vec3 reflectedVertex = normalize(reflect(direction, normalize(Normal))); // we want ot know where the light ray is reflected around the normal. The first argument is what we want ot reflect and the scd is what we are reflecting it around. 
+        vec3 reflectedVertex = reflect(direction, normalize(Normal)); // we want ot know where the light ray is reflected around the normal. The first argument is what we want ot reflect and the scd is what we are reflecting it around. 
         // we are reflecting the light direction across the normal pointing out of the object. 
         // if fragToEye is the same as reflectedVertex then we will see bright light because specular is most intance at that point. 
 
@@ -93,8 +111,9 @@ vec4 CalcDirectionalLight()
 } 
 
 vec4 CalcPointLight(PointLight pLight)
-// to calculate only one of the light so that if we are doing spotLight and it needs pointLight we can use this function
 {
+    vec4 totalColor = vec4(0.0, 0.0, 0.0, 0.0);
+
     vec3 direction = FragPos - pLight.position; // we get the vector from the pointLight to the fragment = direction
     float distance = length(direction); 
     direction = normalize(direction);
@@ -102,8 +121,7 @@ vec4 CalcPointLight(PointLight pLight)
     vec4 color = CalcLightByDirection(pLight.base, direction); 
     float attenuation = pLight.exponent * distance * distance + pLight.linear*distance + pLight.constant;
 
-    return (color/attenuation); // get the color of the pixel 
-
+    return (color/attenuation); 
 }
 
 vec4 CalcSpotLight(SpotLight sLight)
@@ -157,6 +175,158 @@ vec4 CalcSpotLights()
     return totalColor; 
 }
 
+// Vector form without project to the plane (dot with the normal)
+// Use for proxy sphere clipping
+vec3 IntegrateEdgeVec(vec3 v1, vec3 v2)
+{
+    // Using built-in acos() function will result flaws
+    // Using fitting result for calculating acos()
+    float x = dot(v1, v2);
+    float y = abs(x);
+
+    float a = 0.8543985 + (0.4965155 + 0.0145206*y)*y;
+    float b = 3.4175940 + (4.1616724 + y)*y;
+    float v = a / b;
+
+    float theta_sintheta = (x > 0.0) ? v : 0.5*inversesqrt(max(1.0 - x*x, 1e-7)) - v;
+
+    return cross(v1, v2)*theta_sintheta;
+}
+
+float IntegrateEdge(vec3 v1, vec3 v2)
+{
+    return IntegrateEdgeVec(v1, v2).z;
+}
+
+// P is fragPos in world space (LTC distribution)
+vec3 LTC_Evaluate(vec3 N, vec3 V, vec3 P, mat3 Minv, vec3 points[4], bool twoSided)
+{
+    // construct orthonormal basis around N
+    vec3 T1, T2;
+    T1 = normalize(V - N * dot(V, N));
+    T2 = cross(N, T1);
+
+    // rotate area light in (T1, T2, N) basis
+    Minv = Minv * transpose(mat3(T1, T2, N));
+
+    // polygon (allocate 4 vertices for clipping)
+    vec3 L[4];
+    // transform polygon from LTC back to origin Do (cosine weighted)
+    L[0] = Minv * (points[0] - P);
+    L[1] = Minv * (points[1] - P);
+    L[2] = Minv * (points[2] - P);
+    L[3] = Minv * (points[3] - P);
+
+    // use tabulated horizon-clipped sphere
+    // check if the shading point is behind the light
+    vec3 dir = points[0] - P; // LTC space
+    vec3 lightNormal = cross(points[1] - points[0], points[3] - points[0]);
+    bool behind = (dot(dir, lightNormal) < 0.0);
+
+    // cos weighted space
+    L[0] = normalize(L[0]);
+    L[1] = normalize(L[1]);
+    L[2] = normalize(L[2]);
+    L[3] = normalize(L[3]);
+
+    // integrate
+    vec3 vsum = vec3(0.0);
+    vsum += IntegrateEdgeVec(L[0], L[1]);
+    vsum += IntegrateEdgeVec(L[1], L[2]);
+    vsum += IntegrateEdgeVec(L[2], L[3]);
+    vsum += IntegrateEdgeVec(L[3], L[0]);
+
+    // form factor of the polygon in direction vsum
+    float len = length(vsum);
+
+    float z = vsum.z/len;
+    if (behind)
+        z = -z;
+
+    vec2 uv = vec2(z*0.5f + 0.5f, len); // range [0, 1]
+    uv = uv*LUT_SCALE + LUT_BIAS;
+
+    // Fetch the form factor for horizon clipping
+    float scale = texture(LTC2, uv).w;
+
+    float sum = len*scale;
+    if (!behind && !twoSided)
+        sum = 0.0;
+
+    // Outgoing radiance (solid angle) for the entire polygon
+    vec3 Lo_i = vec3(sum, sum, sum);
+    return Lo_i;
+}
+
+// PBR-maps for roughness (and metallic) are usually stored in non-linear
+// color space (sRGB), so we use these functions to convert into linear RGB.
+vec3 PowVec3(vec3 v, float p)
+{
+    return vec3(pow(v.x, p), pow(v.y, p), pow(v.z, p));
+}
+
+const float gamma = 1.0;
+vec3 ToLinear(vec3 v) { return PowVec3(v, gamma); }
+vec3 ToSRGB(vec3 v)   { return PowVec3(v, 1.0/gamma); }
+
+vec4 CalcAreaLights(){
+    // gamma correction
+    if (areaLightCount>0){
+        vec3 mDiffuse = texture(material.diffuse, TexCoord).xyz;// * vec3(0.7f, 0.8f, 0.96f);
+        vec3 mSpecular = ToLinear(vec3(0.23, 0.23, 0.23)); // mDiffuse
+
+        vec3 result = vec3(0.0);
+
+        vec3 N = normalize(Normal);
+        vec3 V = normalize(eyePosition - FragPos);
+        vec3 P = FragPos;
+        float dotNV = clamp(dot(N, V), 0.0, 1.0);
+
+        // use roughness and sqrt(1-cos_theta) to sample M_texture
+        vec2 uv = vec2(material.albedoRoughness.w, sqrt(1.0 - dotNV));
+        uv = uv*LUT_SCALE + LUT_BIAS;
+
+        // get 4 parameters for inverse_M
+        vec4 t1 = texture(LTC1, uv);
+
+        // Get 2 parameters for Fresnel calculation
+        vec4 t2 = texture(LTC2, uv);
+
+        mat3 Minv = mat3(
+            vec3(t1.x, 0.0, t1.y),
+            vec3(  0.0,  1.0,    0.0),
+            vec3(t1.z, 0.0, t1.w)
+        );
+
+        // // translate light source for testing
+        // vec3 translatedPoints[4];
+        // translatedPoints[0] = areaLight.points[0] + areaLightTranslate;
+        // translatedPoints[1] = areaLight.points[1] + areaLightTranslate;
+        // translatedPoints[2] = areaLight.points[2] + areaLightTranslate;
+        // translatedPoints[3] = areaLight.points[3] + areaLightTranslate;
+        for (int i = 0; i < areaLightCount; i++){
+            // Evaluate LTC shading
+            // vec3 diffuse = LTC_Evaluate(N, V, P, mat3(1), translatedPoints, areaLight.twoSided);
+            // vec3 specular = LTC_Evaluate(N, V, P, Minv, translatedPoints, areaLight.twoSided);
+            vec3 diffuse = LTC_Evaluate(N, V, P, mat3(1), areaLights[i].points, areaLights[i].twoSided);
+		    vec3 specular = LTC_Evaluate(N, V, P, Minv, areaLights[i].points, areaLights[i].twoSided);
+            // GGX BRDF shadowing and Fresnel
+            // t2.x: shadowedF90 (F90 normally it should be 1.0)
+            // t2.y: Smith function for Geometric Attenuation Term, it is dot(V or L, H).
+            specular *= mSpecular*t2.x + (1.0f - mSpecular) * t2.y;
+
+            result += areaLights[i].base.color * areaLights[i].base.diffuseIntensity * (specular + mDiffuse * diffuse); // (specular + mDiffuse * diffuse)
+        }
+        // return vec4(1.0);
+
+        // fragColor = vec4(ToSRGB(result), 1.0f);
+        // return vec4(clamp(result, 0.0, 1.0), 1.0);
+        return vec4(ToSRGB(result), 1.0f);
+    }else {
+        return vec4(1.0);
+    }
+}
+
 void main(){
 
     vec4 finalColor = CalcDirectionalLight();
@@ -164,20 +334,7 @@ void main(){
     finalColor += CalcPointLights();
     finalColor += CalcSpotLights(); 
 
-    color = finalColor;
+    finalColor += CalcAreaLights();
+
+    color = texture(theTexture, TexCoord)*finalColor;
 }
-
-
-
-
-
-
-
-//#version 330 core
-//out vec4 FragColor;
-//precision mediump float;
-//in vec4 vertexColor; 
-//void main() {
-//FragColor = vec4(vertexColor.xyz, 1.0);
-//FragColor = vertexColor; 
-//}
--- src/Shaders/vertSrc.vs
+++ src/Shaders/vertSrc.vs
@@ -1,61 +1,22 @@
+#version 330 core
 
-#version 330
-in vec3 pos;
-in vec3 norm; // normal of the vertex we are looking at, it as a direction
-// we will define the normalas as an average of each vertex. 
+in vec3 pos; // position
+in vec3 norm; //Normal
+in vec2 tex;
 
+out vec2 TexCoord;
 out vec4 vertexColor;
 out vec3 Normal; 
 out vec3 FragPos; 
 
-uniform mat4 model;
+uniform mat4 model; // M
+uniform mat4 view;
 uniform mat4 projection;
-uniform mat4 view; 
+
 void main(){
     gl_Position = projection * view * model * vec4(pos, 1.0);
-    vertexColor = vec4(clamp(pos, 0.0, 1.0), 1.0);
-
+    vertexColor = vec4(pos, 1.0);
+    TexCoord = tex;
     Normal = mat3(transpose(inverse(model))) * norm; 
-    // Normal is in relation to where the model is, because if we move the light around we are lihghting objects in different ways. 
-    // If we rotate the object the normal doe not move, or if we scale an object the normal will bend so we need to multiply norm by 
-    // the transform of the model. 
-    // if we scale in only one direction it will also create some problems: the normal will start to point upwards
-    // so if we scale the object uniformly the normals stay in the same direction but if we scale in one dir. the normals start to change dir. 
-    // to solve this we take the inverse and take the transpose => invert scaling process
-
-    FragPos =  (model * vec4(pos, 1.0)).xyz; // we only need to know where it is in the world for the light. 
-    // we want vec3 not vec4 we take .xyz
+    FragPos =  (model * vec4(pos, 1.0)).xyz;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#version 330 core
-
-//in vec3 position;
-//in vec3 normal;
-
-//out vec4 vertexColor; 
-
-//uniform mat4 M;
-//uniform mat4 V;
-//uniform mat4 P;
-
-
-//void main(){
-//    gl_Position = P*V*M*vec4(position, 1.0);
-//    vertexColor = vec4(clamp(position, 0.0, 1.0)*normal, 1.0);
-//}
